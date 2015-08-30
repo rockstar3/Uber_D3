116104d90b4a0fd995da4b6126c2945b26adf519
root <kolja.schneider1125@gmail.com>
2015-08-30 19:37:46 +0200

    first

---
+++ assets/wtid.js
@@ -0,0 +1,3 @@
+if (typeof(Webtrends.dcss.dcsobj_0.dcsGetIdCallback)=="function"){
+Webtrends.dcss.dcsobj_0.dcsGetIdCallback({"gWtId":"","gTempWtId":"e970a82e-2af2-4ab9-8a57-54f9206bda98","gWtAccountRollup":""});
+}
